b91d2aad4486147ea0526ea9c5f715d118bb5d11
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-12-10 22:46:02 -0800

    testing IntelliJ and Java Classes

--- tech-news-java-api/src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ tech-news-java-api/src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechNewsJavaApiApplication {
+public class HelloWorld {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechNewsJavaApiApplication.class, args);
+		SpringApplication.run(HelloWorld.class, args);
 	}
 
 }
+
