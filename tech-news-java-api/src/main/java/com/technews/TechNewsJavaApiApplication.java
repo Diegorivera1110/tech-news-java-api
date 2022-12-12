f5b1d13e56c9fcab7b81727bb27ae63239d2a3eb
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-12-11 17:11:30 -0800

    restarted from scratch

--- tech-news-java-api/src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ tech-news-java-api/src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorld {
+public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorld.class, args);
+		SpringApplication.run(TechNewsJavaApiApplication.class, args);
 	}
 
 }
-
