cdb5080c933b707ab8c60df414ffe0e9ba85ea87
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-12-20 20:14:14 -0800

    testing Java vaiables

---
+++ tech-news-java-api/src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,7 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        int age = 28;
+        double wifeAge = 27.5;
+        double ageDifference = age - wifeAge;
+    }
+}
